d02e3dc4c4dcba20bbafff4e1a38956744808258
Manuel Salcedo Alonso <msalcedo@f-l3202-pc01.aulas.etsit.urjc.es>
2020-02-24 11:07:10 +0100

    Ej2

--- P2/ejemplo2/ejemplo-2.js
+++ P2/ejemplo2/ejemplo-2.js
@@ -5,7 +5,7 @@ const test = document.getElementById('test')
 test.onclick = () => {
   console.log("Click!!");
   if (test.style.backgroundColor=="") {
-      test.style.backgroundColor = "yelow";
+      test.style.backgroundColor = "yellow";
   } else {
     test.style.backgroundColor = "";
   }
