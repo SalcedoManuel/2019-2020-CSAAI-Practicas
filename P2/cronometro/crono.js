0f8dcbe7f9fe64d07453cf01a9dbfbeedbab2a78
Manuel Salcedo Alonso <msalcedo@f-l3210-pc08.aulas.etsit.urjc.es>
2020-02-25 10:42:55 +0100

    crono

---
+++ P2/cronometro/crono.js
@@ -0,0 +1,17 @@
+
+const crono = {
+  cont: 0,
+  seg:0,
+  min:0,
+
+  timer: null,
+  inc: function() {
+
+  }
+  start: function(){
+  }
+  stop: function(){
+  }
+  gui.start.onclick = () {
+  }
+}
