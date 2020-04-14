f7aab9d71867acc3cff1b6f656ba7dcad422f41c
Manuel Salcedo <60213411+SalcedoManuel@users.noreply.github.com>
2020-04-14 10:19:51 +0200

    p18

--- P3/Pong v0.3/pong-03.js
+++ P3/Pong v0.3/pong-03.js
@@ -10,18 +10,32 @@ console.log(`canvas Anchura: ${canvas.width}, Altura: ${canvas.height}`);
 //-- Obtener el contexto para pintar en el canvas
 const ctx = canvas.getContext("2d");
 
+//-- Variables para el marcador
+let count_1 = 0;
+let count_2 = 0;
+
 //--Variables para la Bola
-let bola_x = 50;
+var init_bola_x = 300;
+var init_bola_y = 200;
+let bola_x = init_bola_x;
+let bola_y = init_bola_y;
 let bola_vx = 0;
-let raqI_y = 100;
+let bola_vy = 0;
+
+//--Variables para la raqueta Izquierda
+let raqI_y = 200;
 let raqI_v = 0;
+
+//--Variables para la raqueta Derecha
+let raqD_y = 200;
+let raqD_v = 0;
 
 //-- Pintar todos los objetos en el canvas
 function draw() {
   //-- Dibujar la Bola
   ctx.beginPath();
   ctx.fillStyle='white'
-  ctx.rect(bola_x, 200, 10, 10);
+  ctx.rect(bola_x, bola_y, 10, 10);
   ctx.fill();
 
   //--Dibujar las raquetas
@@ -30,7 +44,7 @@ function draw() {
   //--Raqueta Izquierda
   ctx.rect(50, raqI_y, 10, 40);
   //--Raqueta Derecha
-  ctx.rect(550,300,10,40);
+  ctx.rect(550,raqD_y,10,40);
   ctx.fill();
 
 
@@ -51,20 +65,76 @@ function draw() {
   //---Tanteo
   ctx.font ="100px Arial";
   ctx.fillStyle = "white";
-  ctx.fillText("0", 200, 80);
-  ctx.fillText("1", 340, 80);
+  ctx.fillText(count_1, 200, 80); //Contador Jugador 1, Pos Eje X, Pos Eje Y;
+  ctx.fillText(count_2, 340, 80); //Contador Jugador 2, Pos Eje X, Pos Eje Y;
 }
 
+function goal()
+{
+  bola_x = 300;
+  bola_y = 200;
+  bola_vx = bola_vx * -1;
+  bola_vy = bola_vy * -1;
+}
+function init()
+{
+  bola_x = 300;
+  bola_y = 200;
+  bola_vx = 1;
+  bola_vy = -1;
+}
+function marcador_bola()
+{
+  if(bola_x >= (canvas.width - 40)) {
+    goal();
+    count_1 += 1;
+  }else if (bola_x < 40) {
+    goal();
+    count_2 += 1;
+  }else if ((bola_y < 0)||(bola_y> canvas.height)) {
+    bola_vy = bola_vy * (-1);
+  }
+  //-- Incrementar la posicion x y la y de la bola
+  bola_x += bola_vx;
+  bola_y += bola_vy;
+}
+
+function raqueta()
+{
+  if (raqI_y > (canvas.height - 40)) {
+    raqI_y = (canvas.height - 40);
+    //raqI_v = 0;
+  }else if (raqI_y < 0) {
+    raqI_y = 0;
+  }
+
+  if (raqD_y > (canvas.height - 40)) {
+    raqD_y = (canvas.height - 40);
+  }else if (raqD_y < 0) {
+    raqD_y = 0;
+  }
+  //-- Incrementar la posicion de la raqueta.
+  raqI_y += raqI_v;
+  raqD_y += raqD_v;
+  raqD_v = 0;
+  raqI_v = 0;
+}
+function rebote() {
+  if (((bola_y >= raqI_y) && (bola_y <= (raqI_y + 40))) && (bola_x <= 50)) {
+    bola_vx = bola_vx * (-1);
+  }else if (((bola_y >= raqD_y) && (bola_y <= (raqD_y + 40))) && (bola_x >= 550)) {
+    bola_vx = bola_vx * (-1);
+  }
+}
 //-- Bucle principal de la animación
 function animacion()
 {
-  if(bola_x >= canvas.width) {
-    bola_vx = bola_vx * -1;
-  }
-  //-- Incrementar la posicion x de la bola
-  bola_x += bola_vx;
-  //-- Incrementar la posicion de la raqueta.
-  raqI_y += raqI_v;
+  //--Funcion que lleva el contador y la bola
+  marcador_bola()
+  //--Función de la raqueta
+  raqueta()
+  //--Funcion rebote en la pala
+  rebote()
   //--Borrar el canvas
   ctx.clearRect(0,0, canvas.width, canvas.height);
 
@@ -76,29 +146,25 @@ setInterval(()=>{
   animacion();
 },16);
 
-//--Obtener el botón de dar un "paso"
-const sacar = document.getElementById("sacar");
 
-//Movemos la bola 5 pixeles
-sacar.onclick = () => {
-  //-- Incrementar la posicion x de la bola
-  bola_x = 50;
-  bola_vx = 3;
-  console.log("Saque!");
-}
 window.onkeydown = (e) => {
   //--Comprobar si la tecla es un espacio
   switch (e.key) {
-    case "a":
-      raqI_v = 3;
+    case "s":
+      raqI_v = 10;
       break;
-    case "q":
-      raqI_v = -3;
+    case "w":
+      raqI_v = -10;
+      break;
+    case 'ArrowDown':
+      raqD_v = 10;
+      break;
+    case 'ArrowUp':
+      raqD_v = -10;
       break;
     case " ":
-    bola_x = 50;
-    bola_vx = 3;
-    console.log("Saque!");
+      init();
+      console.log("Saque!");
       break;
     default:
 
